b9735e34d4d8a7be50fecfcb753ee23694a7d994
muncy <muncy.hou@touchcity.tw>
2023-03-08 10:37:03 +0800

    feat: #3 - Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
-<script setup>
-import { RouterView } from 'vue-router'
-</script>
-
 <template>
-  <main class="container">
-    <h1 class="text-weather-secondary font-Roboto-Mono">Whereas disregard and contempt for human rights have resulted</h1>
+  <div class="bg-weather-primary flex min-h-screen flex-col font-Roboto-Mono">
+    <SiteNavigation />
     <RouterView />
-  </main>
+  </div>
 </template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
+</script>
---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav
+      class="container flex flex-col items-center gap-3 py-6 text-2xl text-white sm:flex-row"
+    >
+      <RouterLink :to="{ name: 'home' }" class="flex items-center gap-3">
+        <i class="fa-solid fa-sun"></i>
+        <h1>The Local Weather</h1>
+      </RouterLink>
+
+      <div class="flex gap-3 sm:ml-auto">
+        <a
+          href="javascript: void(0)"
+          class="duration-150 hover:text-weather-secondary"
+        >
+          <i class="fa-solid fa-circle-info"></i>
+        </a>
+        <a
+          href="javascript: void(0)"
+          class="duration-150 hover:text-weather-secondary"
+        >
+          <i class="fa-solid fa-plus"></i>
+        </a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
